5c99325a43052a4a7339010631569c51fdb26308
FireFlyDonut123 <162411910+FireFlyDonut123@users.noreply.github.com>
2024-04-22 00:08:25 +0800

    Add files via upload

--- scriptBlog.js
+++ scriptBlog.js
@@ -1,50 +1,50 @@
-var currentGallery = 1; // Menyimpan nomor galeri yang sedang ditampilkan
-var totalGalleries = 2; // Jumlah total galeri
-var currentGalleryContent = ""; // Menyimpan konten galeri yang sedang ditampilkan
+var currentBlog = 1; 
+var totalBlog = 2; 
+var currentBlogContent = ""; 
 
-function loadGallery() {
-    var xhr = new XMLHttpRequest(); // Buat objek XMLHttpRequest
+function loadBlog() {
+    var xhr = new XMLHttpRequest(); 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            // Ketika permintaan berhasil dan respons diterima dengan baik
-            currentGalleryContent = this.responseText;
-            document.getElementById("blogContainer").innerHTML = currentGalleryContent;
-            currentGallery = 1; // Set galeri yang sedang ditampilkan menjadi galeri 1
-            toggleButtons(); // Periksa tombol mana yang harus ditampilkan
-            document.getElementById("loadButton").style.display = "none"; // Sembunyikan tombol "Load Gallery"
+           
+            currentBlogContent = this.responseText;
+            document.getElementById("blogContainer").innerHTML = currentBlogContent;
+            currentBlog = 1; 
+            toggleButtons(); 
+            document.getElementById("loadButton").style.display = "none"; 
         }
     };
-    xhr.open("GET", "blog1.html", true); // Muat galeri 1 saat tombol "Load Gallery" ditekan
-    xhr.send(); // Kirim permintaan
+    xhr.open("GET", "blog1.html", true); 
+    xhr.send(); 
 }
 
-function nextGallery() {
-    currentGallery = currentGallery === totalGalleries ? 1 : currentGallery + 1; // Toggle antara galeri 1 dan 2
-    loadGalleryContent();
+function nextBlog() {
+    currentBlog = currentBlog === totalBlog ? 1 : currentBlog + 1; 
+    loadBlogContent();
 }
 
-function previousGallery() {
-    currentGallery = currentGallery === 1 ? totalGalleries : currentGallery - 1; // Toggle antara galeri 1 dan 2
-    loadGalleryContent();
+function previousBlog() {
+    currentBlog = currentBlog === 1 ? totalGalleries : currentBlog - 1; 
+    loadBlogContent();
 }
 
-function loadGalleryContent() {
-    var xhr = new XMLHttpRequest(); // Buat objek XMLHttpRequest
+function loadBlogContent() {
+    var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            // Ketika permintaan berhasil dan respons diterima dengan baik
-            currentGalleryContent = this.responseText;
-            document.getElementById("blogContainer").innerHTML = currentGalleryContent;
-            toggleButtons(); // Periksa tombol mana yang harus ditampilkan
+         
+            currentBlogContent = this.responseText;
+            document.getElementById("blogContainer").innerHTML = currentBlogContent;
+            toggleButtons();
         }
     };
-    xhr.open("GET", "blog" + currentGallery + ".html", true); // Muat galeri yang sedang aktif
-    xhr.send(); // Kirim permintaan
+    xhr.open("GET", "blog" + currentBlog + ".html", true); 
+    xhr.send(); 
 }
 
 function toggleButtons() {
-    // Periksa galeri yang sedang ditampilkan dan tampilkan tombol yang sesuai
-    if (currentGallery === 1) {
+   
+    if (currentBlog === 1) {
         document.getElementById("previousButton").style.display = "none";
         document.getElementById("nextButton").style.display = "inline-block";
     } else {
--- scriptGallery.js
+++ scriptGallery.js
@@ -1,49 +1,49 @@
-var currentGallery = 1; // Menyimpan nomor galeri yang sedang ditampilkan
-var totalGalleries = 2; // Jumlah total galeri
-var currentGalleryContent = ""; // Menyimpan konten galeri yang sedang ditampilkan
+var currentGallery = 1;
+var totalGalleries = 2; 
+var currentGalleryContent = ""; 
 
 function loadGallery() {
-    var xhr = new XMLHttpRequest(); // Buat objek XMLHttpRequest
+    var xhr = new XMLHttpRequest(); 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            // Ketika permintaan berhasil dan respons diterima dengan baik
+            
             currentGalleryContent = this.responseText;
             document.getElementById("galleryContainer").innerHTML = currentGalleryContent;
-            currentGallery = 1; // Set galeri yang sedang ditampilkan menjadi galeri 1
-            toggleButtons(); // Periksa tombol mana yang harus ditampilkan
-            document.getElementById("loadButton").style.display = "none"; // Sembunyikan tombol "Load Gallery"
+            currentGallery = 1; 
+            toggleButtons(); 
+            document.getElementById("loadButton").style.display = "none"; 
         }
     };
-    xhr.open("GET", "gallery1.html", true); // Muat galeri 1 saat tombol "Load Gallery" ditekan
-    xhr.send(); // Kirim permintaan
+    xhr.open("GET", "gallery1.html", true); 
+    xhr.send();
 }
 
 function nextGallery() {
-    currentGallery = currentGallery === totalGalleries ? 1 : currentGallery + 1; // Toggle antara galeri 1 dan 2
+    currentGallery = currentGallery === totalGalleries ? 1 : currentGallery + 1; 
     loadGalleryContent();
 }
 
 function previousGallery() {
-    currentGallery = currentGallery === 1 ? totalGalleries : currentGallery - 1; // Toggle antara galeri 1 dan 2
+    currentGallery = currentGallery === 1 ? totalGalleries : currentGallery - 1;
     loadGalleryContent();
 }
 
 function loadGalleryContent() {
-    var xhr = new XMLHttpRequest(); // Buat objek XMLHttpRequest
+    var xhr = new XMLHttpRequest(); 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            // Ketika permintaan berhasil dan respons diterima dengan baik
+            
             currentGalleryContent = this.responseText;
             document.getElementById("galleryContainer").innerHTML = currentGalleryContent;
-            toggleButtons(); // Periksa tombol mana yang harus ditampilkan
+            toggleButtons(); 
         }
     };
-    xhr.open("GET", "gallery" + currentGallery + ".html", true); // Muat galeri yang sedang aktif
-    xhr.send(); // Kirim permintaan
+    xhr.open("GET", "gallery" + currentGallery + ".html", true); 
+    xhr.send(); 
 }
 
 function toggleButtons() {
-    // Periksa galeri yang sedang ditampilkan dan tampilkan tombol yang sesuai
+   
     if (currentGallery === 1) {
         document.getElementById("previousButton").style.display = "none";
         document.getElementById("nextButton").style.display = "inline-block";
